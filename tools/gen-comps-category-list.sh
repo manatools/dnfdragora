d24c4f9f0ee859fc400520622bf6223c17135da6
Adam Williamson <awilliam@redhat.com>
2023-10-06 17:00:46 -0700

    Fix the gen-comps-category-list.sh script
    There's no 'master' branch any more, and F26 is pretty old.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tools/gen-comps-category-list.sh
+++ tools/gen-comps-category-list.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Generate the map from category name to ID for dnfdragora/compsicons.py
 # Workaround for https://github.com/timlau/dnf-daemon/issues/9
-wget https://pagure.io/fedora-comps/raw/master/f/comps-f26.xml.in -q -O - | tr '\n' '#' | sed -e 's!<group>.*</group>!!g' -e 's!<environment>.*</environment>!!g' | tr '#' '\n' | grep '<_name>\|<id>' | tr '\n' '#' | sed -e 's!<id>\([^<]*\)</id># *<_name>\([^<]*\)</_name>!"\2": \{"title": _\("\2"\), "icon" :"\1.png"\},!g' | tr '#' '\n' | sed -e 's/^ */            /g'
+wget https://pagure.io/fedora-comps/raw/main/f/comps-f40.xml.in -q -O - | tr '\n' '#' | sed -e 's!<group>.*</group>!!g' -e 's!<environment>.*</environment>!!g' | tr '#' '\n' | grep '<_name>\|<id>' | tr '\n' '#' | sed -e 's!<id>\([^<]*\)</id># *<_name>\([^<]*\)</_name>!"\2": \{"title": _\("\2"\), "icon" :"\1.png"\},!g' | tr '#' '\n' | sed -e 's/^ */            /g'
